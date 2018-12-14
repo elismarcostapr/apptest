c2402e1acd33b184ba1d1de1c3e2247c9106120c
elismarcostapr <45858840+elismarcostapr@users.noreply.github.com>
2018-12-14 14:42:11 -0300

    acabamos sidnav e routas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,23 @@ import { HomeComponent } from './components/home/home.component'
 import { FinaceiroComponent } from './modules/finaceiro/finaceiro.component';
 import { FiscalComponent } from './modules/fiscal/fiscal.component';
 import { PessoasComponent } from './modules/pessoas/pessoas.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { PrivadoComponent } from './components/privado/privado.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+    import { from } from 'rxjs';
 
 const appRoutes: Routes = [{ path: '', component: HomeComponent},
 
 { path: 'financeiro', component: FinaceiroComponent },
 { path: 'fiscal', component: FiscalComponent },
 { path: 'pessoas', component: PessoasComponent },
+{ path: 'login', component: LoginComponent },
+{ path: 'register', component: RegisterComponent },
+{ path: 'privado', component: PrivadoComponent },
+{ path: '**', component: NotFoundComponent },
 
-// otherwise redirect to home
-{ path: '**', redirectTo: '' }];
+];
 
 export const routing = RouterModule.forRoot(appRoutes);
